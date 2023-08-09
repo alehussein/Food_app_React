b06bd2934f5a04cb6590e68cb88c89ee7126ed89
Alejandro Hussein <alejandrohussein1@gmail.com>
2023-08-08 21:33:57 -0500

    validate_Form

--- src/Componets/Cart/Checkout.js
+++ src/Componets/Cart/Checkout.js
@@ -1,36 +1,144 @@
-import React from "react"
+import React, { useState } from "react"
 import classes from './Checkout.module.css';
 
 
 const Checkout = (props) => {
 
-  const confirmHandler = (evt) => {
+
+  const [input, setInput] = useState({
+    name: '',
+    street: '',
+    postal: '',
+    city: '',
+  })
+
+  const [validate, setValidate] = useState({
+    vName: true,
+    vStreet: true,
+    vPostal: true,
+    vCity: true
+  })
+
+  const { name, street, postal, city } = input
+
+  const validateName = name ? input.name.trim() !== '' : false;
+  const validateStreet = street ? input.street.trim().length !== 0 : false;
+  const validatePostal = postal ? input.postal.trim().length !== 0 : false;
+  const validateCity = city ? input.city.trim().length !== 0 : false;
+
+  const formIsValid =
+    validateName &&
+    validateStreet &&
+    validatePostal &&
+    validateCity
+
+  const confirmHandler = (evt) => {    ///OK
     evt.preventDefault()
+    setValidate({
+      vName: validateName,
+      vStreet: validateStreet,
+      vPostal: validatePostal,
+      vCity: validateCity
+    })
+
+    if (!formIsValid) {
+      return;
+    }
   }
 
+
+  const onBlurHandler = (fieldName, target) => {
+    if (!input[target]) {
+      setValidate((prev) => ({
+        ...prev,
+        [fieldName]: false,
+      }));
+    } else {
+      setValidate((prev) => ({
+        ...prev,
+        [fieldName]: true,
+      }));
+    }
+  };
+
+  const inputHandler = (evt) => {
+    const { name, value } = evt.target
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  const nameControl = `${classes.control} ${validate.vName ? '' : classes.invalid}`;
+  const streetControl = `${classes.control} ${validate.vStreet ? '' : classes.invalid}`;
+  const postalControl = `${classes.control} ${validate.vPostal ? '' : classes.invalid}`;
+  const cityControl = `${classes.control} ${validate.vCity ? '' : classes.invalid}`;
+
+
+
   return (
-    <form onSubmit={confirmHandler}>
-      <div className={classes.control}>
-        <label htmlFor="name">Your Name</label>
-        <input type='text' id="name" />
+    <form onSubmit={confirmHandler} className={classes.form}>
+      <div className={nameControl}>
+        <label
+          htmlFor="name">
+          {validate.vName ? 'Your Name' : 'Enter a Valid name'}
+        </label>
+        <input
+          type='text' id="name"
+          value={input.name}
+          name='name'
+          onChange={inputHandler}
+          onBlur={() => onBlurHandler('vName', 'name')}
+        />
       </div>
-      <div className={classes.control}>
-        <label htmlFor="street">Street</label>
-        <input type='text' id="street" />
+      <div className={streetControl}>
+        <label
+          htmlFor="street">
+          {validate.vStreet ? 'Street' : 'Enter you address'}
+        </label>
+        <input
+          type='text'
+          id="street"
+          value={input.street}
+          name='street'
+          onChange={inputHandler}
+          onBlur={() => onBlurHandler('vStreet', 'street')}
+        />
       </div>
-      <div className={classes.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input type='text' id="postal" />
+      <div className={postalControl}>
+        <label
+          htmlFor="postal">
+          {validate.vPostal ? 'Postal Code' : 'Enter you Postal Code'}
+        </label>
+        <input
+          type='text'
+          id="postal"
+          value={input.postal}
+          name='postal'
+          onChange={inputHandler}
+          onBlur={() => onBlurHandler('vPostal', 'postal')}
+        />
       </div>
-      <div className={classes.control}>
-        <label htmlFor="city">City</label>
-        <input type='text' id="city" />
+      <div className={cityControl}>
+        <label
+          htmlFor="city">
+          {validate.vCity ? 'City' : 'Enter city'}
+        </label>
+        <input
+          type='text'
+          id="city"
+          value={input.city}
+          name='city'
+          onChange={inputHandler}
+          onBlur={() => onBlurHandler('vCity', 'city')} />
       </div>
       <div className={classes.actions}>
-      <button className={classes.button}>Confirm</button>
-      <button type="button" className={classes['button--alt']} onClick={props.onCancel}>
-        Cancel
+        <button type="button" className={classes.button} onClick={props.onCancel}>
+          Cancel
         </button>
+        <button
+          disabled={!formIsValid}
+          className={classes.submit}>Confirm</button>
       </div>
     </form>
   )
